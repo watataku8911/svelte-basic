9133fcbb8e4026dd02ef9240db79f8852e21e17a
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-08-15 14:59:48 +0900

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+	const messages = ['Hello!', 'Svelte!'];
+	let current = 0;
+
+	const toggle = () => current = (current+1) % 2;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{messages[current]}
+	<button on:click={toggle}>Toggle Message</button>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
